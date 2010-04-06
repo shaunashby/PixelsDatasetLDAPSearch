4185cb9651adaaff834dac83078f46ce5374d46f
Shaun Ashby <Shaun.Ashby@unige.ch>
2010-04-06 17:31:20 +0200

    First working version. Just dump all DNs for starters.

---
+++ main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include "ldap.h"
+
+/* Server name: */
+#define HOST "ashby.isdc.unige.ch"
+/* The user and passwd to bind to the server with: */
+#define BINDDN "cn=Manager,dc=ashby,dc=isdc,dc=unige,dc=ch"
+#define BINDPASSWD "dummy+999"
+
+/* Parameters for searches: */
+#define BASEDN "ou=Datasources,dc=ashby,dc=isdc,dc=unige,dc=ch"
+#define SCOPE LDAP_SCOPE_SUBTREE
+#define QUERY_FILTER "(&(objectClass=integralDS))"
+
+int main (int argc, const char * argv[]) {
+	
+	int ldp_result=0;
+	LDAPMessage *result, *e;
+	
+	char *dn;
+	
+	/*Set up the connection to the LDAP server: */
+	LDAP *handle;
+	if ((handle = ldap_init(HOST, LDAP_PORT)) == NULL) {
+		perror( "ldap_init" );
+		return(1);
+	}
+		
+	/* Set the protocol version: */
+	int ldap_version = LDAP_VERSION3;
+	ldap_set_option(handle, LDAP_OPT_PROTOCOL_VERSION, &ldap_version);	
+
+	/* Bind to the server: */
+	ldp_result = ldap_simple_bind_s(handle,BINDDN,BINDPASSWD);
+	if ( ldp_result != LDAP_SUCCESS ) {
+		fprintf(stderr, "ldap_simple_bind_s: %s\n\n", ldap_err2string(ldp_result));
+		return(1);
+	} else {
+		printf( "Bind to %s:%d as DN %s successful.\n",HOST,LDAP_PORT,BINDDN);
+	}
+	
+	/* Run a query: */
+	ldp_result = ldap_search_ext_s(handle,BASEDN,SCOPE,QUERY_FILTER, NULL, 0, NULL, NULL, NULL, 1000, &result);
+	
+	if ( ldp_result != LDAP_SUCCESS ) {
+		fprintf(stderr, "ldap_search_ext_s: %s\n", ldap_err2string(ldp_result));
+		return( 1 );
+	} else {
+		printf( "Search using base DN \"%s\", filter \"%s\" successful.\n",BASEDN,QUERY_FILTER);
+	}
+	
+	for ( e = ldap_first_entry(handle,result); e != NULL; e = ldap_next_entry(handle,e) ) {
+		if ( (dn = ldap_get_dn(handle,e)) != NULL ) {
+			printf("dn: %s\n", dn);
+			ldap_memfree(dn);
+		}
+	}
+	
+	ldap_msgfree( result );
+	
+	/* Unbind when finished: */
+	ldp_result = ldap_unbind_s(handle);
+	return 0;
+}
